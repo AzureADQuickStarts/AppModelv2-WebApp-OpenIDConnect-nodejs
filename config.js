b968e87a9687a947f2ede683926b09d96445015f
Brandon Werner <brandwe@microsoft.com>
2015-08-26 17:16:34 -0700

    Adding dependencies

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  	returnURL: 'http://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
  	realm: 'http://localhost:3000',
- 	clientID: '<Your app ID>',
- 	clientSecret: '<Your secret>'
+ 	clientID: '<your appliction id>',
+ 	clientSecret: '<your secret>'
  };
